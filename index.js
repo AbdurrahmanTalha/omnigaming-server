6b5fd86965ec1c99be96c6ea7258fa194bce715d
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-04-28 22:17:28 +0600

    seted up mongodb and index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -9,6 +10,18 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6vswd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try { 
+        await client.connect()
+        const computerCollection = client.db("Computers").collection("computer")
+    }
+    finally {
+        
+    }
+}
 app.get('/', (req, res) => {
     res.send("Running Genius Server")
 })
