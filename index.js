eff67f090ffed51c2191b663a53b8b9773ca2abc
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-04-29 09:34:26 +0600

    loaded items

--- index.js
+++ index.js
@@ -14,14 +14,24 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6vswd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try { 
+    try {
         await client.connect()
         const computerCollection = client.db("Computers").collection("computer")
+        app.get("/item", async (req, res) => {
+            const query = {};
+            const cursor = computerCollection.find(query);
+            const page = await cursor.toArray();
+            res.send(page)
+        })
+
     }
     finally {
-        
+
     }
 }
+
+
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send("Running Genius Server")
 })
